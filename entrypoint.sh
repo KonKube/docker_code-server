0303482a713a48c5916dc12f265fff67fd9075a8
KonKube <konkube@gmail.com>
2021-10-11 12:41:21 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,10 @@ if [ "${DOCKER_USER-}" ]; then
     echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
     # Unfortunately we cannot change $HOME as we cannot move any bind mounts
     # nor can we bind mount $HOME into a new home as that requires a privileged container.
-    sudo usermod --login "$DOCKER_USER" linuxbrew
-    sudo groupmod -n "$DOCKER_USER" linuxbrew
+    sudo usermod --login "$DOCKER_USER" konkube
+    sudo groupmod -n "$DOCKER_USER" konkube
 
-    sudo sed -i "/linuxbrew/d" /etc/sudoers.d/nopasswd
+    sudo sed -i "/konkube/d" /etc/sudoers.d/nopasswd
   fi
 fi
 
